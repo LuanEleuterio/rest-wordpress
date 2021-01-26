1c0549c40c83b804889bf7622c6a69af869fce1a
LuanEleuterio <luaneleuterio3@hotmail.com>
2021-01-26 15:23:54 -0300

    Adicionado o Loop de post em todas as paginas

--- wp-content/themes/rest/index.php
+++ wp-content/themes/rest/index.php
@@ -1,4 +1,18 @@
 
 <?php get_header(); ?>
-	<p>Looping</p>
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		<section class="container sobre">
+			<h2 class="subtitulo"><?php the_title(); ?></h2>
+
+			<div class="grid-8">
+				<?php the_content(); ?>
+			</div>
+		</section>
+
+	<?php endwhile; else: ?>
+		<section class="container sobre">
+			<h2 class="subtitulo"><?php _e('Sorry, no posts matched your criteria.'); ?></h2>
+		</section>
+
+	<?php endif; ?>
 <?php get_footer(); ?>
